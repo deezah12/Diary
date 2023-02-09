6d9d4bcdc1008b6b9637ba1c9b6cb0dc8b30b89e
Hadiza <deezah30@gmail.com>
2023-02-09 23:04:54 +0100

    token-business-logic

--- src/main/java/com/semicolon/diary/service/inter/TokenService.java
+++ src/main/java/com/semicolon/diary/service/inter/TokenService.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 public interface TokenService {
     void  saveConfirmationToken(Token token);
-    Optional<Token> getConfirmationToken(String token);
+    Token getConfirmationToken(String token);
     void deleteExpiredToken();
     void setTokenConfirmationAt(String token);
 }
